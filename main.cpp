c1010add4a19d0116bc295d771ebceb89831da5e
Volker Enderlein <volkerenderlein@hotmail.com>
2020-12-21 01:28:47 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <OpenMesh/Core/Geometry/QuadricT.hh>
+
+int main() {
+    OpenMesh::Geometry::QuadricT<double> qt;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
